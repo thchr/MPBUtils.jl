cdef573664b668c349bf108ffee0dfa6b4f26115
Ali Ghorashi <75496694+AliGhorashiCMT@users.noreply.github.com>
2022-04-23 11:12:51 -0400

    Update src/read_utils.jl
    Co-authored-by: Thomas Christensen <tchr@mit.edu>

--- src/read_utils.jl
+++ src/read_utils.jl
@@ -88,7 +88,7 @@ function fix_gamma_irrep(symeigsd::Dict{String, Vector{Vector{ComplexF64}}}, lg_
         elseif polarization == "te"
             symeigsd_fixed["Γ"][1] = signs
         end
-    elseif D ==3
+    elseif D == 3
         x2T = signs .* (2cospi.(2 ./ Crystalline.rotation_order.(lg_gamma)) .+ 1) .- 1
         splitting_fraction = 0.35
         symeigsd_fixed[Γ][1] = x2T*splitting_fraction
