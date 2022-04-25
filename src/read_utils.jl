3902ad341d6d098813030c5d026cce7db658752e
Ali Ghorashi <75496694+AliGhorashiCMT@users.noreply.github.com>
2022-04-25 14:36:01 -0400

    add `fixup_gamma_symmetry` method to correct symmetry eigenvalues of zero-frequency photonic bands (#13)
    * Fix gamma irrep
    
    * Convenience method for gamma irreps
    
    * Fix 3d gamma irrep
    
    * splitting fraction
    
    * Update src/read_utils.jl
    
    Co-authored-by: Thomas Christensen <tchr@mit.edu>
    
    * Update src/read_utils.jl
    
    Co-authored-by: Thomas Christensen <tchr@mit.edu>
    
    * Update src/read_utils.jl
    
    Co-authored-by: Thomas Christensen <tchr@mit.edu>
    
    * Update src/read_utils.jl
    
    Co-authored-by: Thomas Christensen <tchr@mit.edu>
    
    * misc tidying up
    
    * doc string and change name to `fixup_gamma_symmetry!`
    
    Co-authored-by: Thomas Christensen <tchr@mit.edu>

--- src/read_utils.jl
+++ src/read_utils.jl
@@ -93,6 +93,78 @@ function Base.show(io::IO, bs::BandSummary) # compact print
     Crystalline.prettyprint_symmetryvector(io, bs.n, irreplabels(bs.brs))
 end
 
+# ---------------------------------------------------------------------------------------- #
+
+"""
+$(TYPEDSIGNATURES)
+
+Mutate the lowest `D-1` Γ-point entries of the provided dictionary of symmetry eigenvalues
+`symeigsd` which correspond to the singular zero-frequency eigenstates. 
+The mutated entries represent a valid, consistent choice of symmetry eigenvalues, following
+the operator sorting of `lg_Γ`. Returns the mutated `symeigsd`.
+
+In `D=2`, the polarization choice (transverse electric `:TE` or transverse magnetic `:TM`)
+must be specified as the `polarization` argument.
+
+## Details
+
+The calculation of the singular zero-frequency symmetry eigenvalues of photonic crystals by
+conventional numerical methods is usually unreliable since the photonic eigenvalue problem
+is singular in the zero-frequency limit. To overcome this problem, the symmetry eigenvalues
+of the `D-1` lowest bands at the Γ-point can be manually corrected, following the procedure
+described in [^1].
+
+**In 3D**, the two lowest-frequency symmetry eigenvalues ("2T") at the Γ-point are set such
+that their sum is (``\\theta`` denoting the rotation angle of an operation ``g`` and
+``det(g)`` its handedness):
+
+``
+x^{\\text{2T}}_{\\Gamma}(g) = \\det(g) (2\\cos(\\theta) + 1) - 1.
+``
+
+In practice, this sum is distributed arbitrarily over the two lowest bands at Γ.
+
+**In 2D**, depending on the choice of `polarization` (either `:TM` or `:TE`), the
+lowest-frequency Γ-point symmetry eigenvalue is set to:
+
+``
+x^{\\text{TM}}_{\\Gamma}(g) = 1, 
+x^{\\text{TE}}_{\\Gamma}(g) = \\det(g).
+``
+
+[1]: Christensen, Po, Joannopoulos, & Soljacic, Location and topology of the fundamental
+     gap in photonic crystals, [arXiv2106.10267 (2021)](arxiv.org/abs/2106.10267).
+"""
+function fixup_gamma_symmetry!(
+            symeigsd::Dict{String, Vector{Vector{ComplexF64}}}, # ← mutated by call
+            lg_Γ::AbstractVector{SymOperation{D}},
+            polarization::Union{Symbol, Nothing} = nothing) where D
+
+    if D == 2
+        if polarization == :TM
+            symeigsd["Γ"][1] .= 1
+        elseif polarization == :TE
+            symeigsd["Γ"][1] .= det.(rotation.(lg_Γ))
+        end
+    elseif D == 3
+        ns = Crystalline.rotation_order.(lg_Γ) # rotation order (abs) and handedness (sign)
+        x2T = sign.(ns) .* (2cospi.(2 ./ ns) .+ 1) .- 1
+        splitting_fraction = 0.3721 # arbitrary fraction of 1 to avoid bands 1 and 2 
+                                    # appearing splittable by accident
+        symeigsd[Γ][1] .= x2T .* splitting_fraction
+        symeigsd[Γ][2] .= x2T .* (1 - splitting_fraction)
+    end
+    return symeigsd
+end
+function fixup_gamma_symmetry!(
+            symeigsd::Dict{String, Vector{Vector{ComplexF64}}}, # ← mutated by call
+            lgd::Dict{String, LittleGroup{D}},
+            polarization::Union{Symbol, Nothing} = nothing) where D
+
+    return fixup_gamma_symmetry!(symeigsd, lgd["Γ"], polarization)
+end
+
+# ---------------------------------------------------------------------------------------- #
 
 """
 $(TYPEDSIGNATURES)
